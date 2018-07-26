37e3258f19850a7057105232af12dbde2ff8333e
林一峰 <1feng.0595@gmail.com>
2018-07-26 22:24:56 +0800

    paginate simple

--- apphome/common/model/Arctype.php
+++ apphome/common/model/Arctype.php
@@ -51,10 +51,11 @@ class Arctype extends Base
      * @param string $order 排序
      * @param string $field 字段
      * @param int $limit 每页几条
+     * @param int|bool $simple 是否简洁模式或者总记录数
      * @param int $page 当前第几页
      * @return array
      */
-    public function getPaginate($where = array(), $order = '', $field = '*', $limit = 15)
+    public function getPaginate($where = array(), $order = '', $field = '*', $limit = 15, $simple = false)
     {
         $res = $this->getDb()->where($where);
         
@@ -67,7 +68,7 @@ class Arctype extends Base
             $res = $res->field($field);
         }
         
-        return $res->order($order)->paginate($limit, false, array('query' => request()->param()));
+        return $res->order($order)->paginate($limit, $simple, array('query' => request()->param()));
     }
     
     /**
